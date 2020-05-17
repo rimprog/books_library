a5dcb2e6f5ee033b916b9ab4be6891db9836b6b7
rimprog <riminprog@gmail.com>
2020-05-17 10:23:06 +0300

    Download image and book info

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
-import requests
 from pathlib import Path
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+from pathvalidate import sanitize_filepath
 
 
 MEDIA_URL = os.path.join("media",'')
 
 
-def get_book_text(book_id):
-    url = 'http://tululu.org/txt.php'
+def get_book_text(url, book_id):
     payload = {'id':book_id}
 
     response = requests.get(url, params=payload, allow_redirects=False)
@@ -19,26 +23,64 @@ def get_book_text(book_id):
     return response.text
 
 
+def get_book_info(url):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    h1_tag = soup.find('h1')
+
+    title_text, author = h1_tag.text.split('::')
+    title_text = sanitize_filename(title_text.strip())
+    author = sanitize_filename(author.strip())
+
+    img_src = soup.find(class_='bookimage').find('img')['src']
+    bookimage_url = urljoin(url, img_src)
+
+    return title_text, author, bookimage_url
+
+
 def save_text_file_to_folder(text_file, filename, folder_path):
     Path(folder_path).mkdir(parents=True, exist_ok=True)
 
-    file_path = folder_path + filename
+    file_path = os.path.join(folder_path, filename)
     with open(file_path, 'w') as file:
         file.write(text_file)
 
 
+def download_image(url, filename, folder_path):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    Path(folder_path).mkdir(parents=True, exist_ok=True)
+
+    file_path = os.path.join(folder_path, filename)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+
 def main():
+    base_url = 'http://tululu.org'
+    book_download_url = os.path.join(base_url, 'txt.php')
+
     book_ids = range(1,11)
     for book_id in book_ids:
-        book_text = get_book_text(book_id)
+        book_text = get_book_text(book_download_url, book_id)
 
         if book_text == None:
             continue
 
-        book_name = 'book_{}.txt'.format(book_id)
-        books_path = os.path.join(MEDIA_URL, 'books','')
+        book_info_url = os.path.join(base_url, 'b{}/'.format(book_id))
+        title_text, author, bookimage_url = get_book_info(book_info_url)
+        book_name = '{}. {}.txt'.format(book_id, title_text)
 
+        books_path = os.path.join(MEDIA_URL, 'books', '')
         save_text_file_to_folder(book_text, book_name, books_path)
+
+        book_image_path = os.path.join(MEDIA_URL, 'images', '')
+        image_name = bookimage_url.split('/')[-1]
+        download_image(bookimage_url, image_name, book_image_path)
 
 
 if __name__ == '__main__':
