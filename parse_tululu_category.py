3991dd80981cd3d72ddba6009c3a9bfef7f4eec9
rimprog <riminprog@gmail.com>
2020-05-18 14:44:52 +0300

    Move all functions from parse_tululu_page.py to parse_tululu_category.py

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -6,9 +6,78 @@ from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+from pathvalidate import sanitize_filepath
 
-from parse_tululu_page import get_book_text, get_book_info
-from parse_tululu_page import MEDIA_URL, save_text_file_to_folder, download_image
+
+MEDIA_URL = os.path.join("media",'')
+
+
+def get_book_text(url, book_id):
+    payload = {'id':book_id}
+
+    response = requests.get(url, params=payload, allow_redirects=False)
+    response.raise_for_status()
+
+    if response.status_code == 302:
+        return None
+
+    return response.text
+
+
+def get_book_info(url):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    h1_tag = soup.select_one('h1')
+
+    title_text, author = h1_tag.text.split('::')
+    title_text = sanitize_filename(title_text.strip())
+    author = sanitize_filename(author.strip())
+
+    image_tag_selector = '.bookimage img[src]'
+    image_tag = soup.select_one(image_tag_selector)
+    image_src = image_tag.get('src')
+    image_url = urljoin(url, image_src)
+
+    comments_tags_selector = '.texts span'
+    comments_tags = soup.select(comments_tags_selector)
+    comments = [comment_tag.text for comment_tag in comments_tags]
+
+    genres_tags_selector = 'span.d_book a'
+    genres_tags = soup.select(genres_tags_selector)
+    genres = [genre_tag.text for genre_tag in genres_tags]
+
+    book_info = {
+        'title_text': title_text,
+        'author': author,
+        'image_url': image_url,
+        'comments': comments,
+        'genres': genres
+    }
+
+    return book_info
+
+
+def save_text_file_to_folder(text_file, filename, folder_path):
+    Path(folder_path).mkdir(parents=True, exist_ok=True)
+
+    file_path = os.path.join(folder_path, filename)
+    with open(file_path, 'w') as file:
+        file.write(text_file)
+
+
+def download_image(url, filename, folder_path):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    Path(folder_path).mkdir(parents=True, exist_ok=True)
+
+    file_path = os.path.join(folder_path, filename)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
 
 
 def parse_category(category_url, start_id, end_id):
@@ -104,8 +173,12 @@ def main():
             image_name = book_info['image_url'].split('/')[-1]
             download_image(book_info['image_url'], image_name, book_image_path)
 
+        img_path = os.path.join(book_image_path, image_name)
+        book_info['img_path'] = img_path
+
         book_path = os.path.join(books_path, book_name)
         book_info['book_path'] = book_path
+
         books_description.append(book_info)
 
         print(book_path)
