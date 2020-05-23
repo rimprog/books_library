d465bd9282a80e0d238f9a8ead58df9060733c19
rimprog <riminprog@gmail.com>
2020-05-23 22:22:05 +0300

    Code refactoring

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -13,6 +13,52 @@ from pathvalidate import sanitize_filepath
 MEDIA_URL = os.path.join("media",'')
 
 
+def configurate_argparse(get_pages_count, category_url):
+    parser = argparse.ArgumentParser(
+        description='This script parse books from categories pages on tululu.org website'
+    )
+    parser.add_argument(
+        '--start_page',
+        type=int,
+        default=1,
+        help='id of start page books in category'
+    )
+    parser.add_argument(
+        '--end_page',
+        type=int,
+        default=get_pages_count(category_url),
+        help='id of end page books in category'
+    )
+    parser.add_argument(
+        '--dest_folder',
+        default=MEDIA_URL,
+        help='path to result of parsing'
+    )
+    parser.add_argument(
+        '--skip_imgs',
+        action='store_true',
+        help='option to allow skip download book\'s images'
+    )
+    parser.add_argument(
+        '--skip_txt',
+        action='store_true',
+        help='option to allow skip download book\'s text'
+    )
+    parser.add_argument(
+        '--json_path',
+        default=MEDIA_URL,
+        help='path to books description json file'
+    )
+
+    return parser
+
+
+def get_books_ids(books_urls):
+    books_ids = [book_url.split('/b')[-1][:-1] for book_url in books_urls]
+
+    return books_ids
+
+
 def get_book_text(url, book_id):
     payload = {'id':book_id}
 
@@ -62,6 +108,12 @@ def get_book_info(url):
     }
 
     return book_info
+
+
+def create_book_name(book_id, book_info):
+    book_name = '{}. {}.txt'.format(book_id, book_info['title_text'])
+
+    return book_name
 
 
 def get_pages_count(url):
@@ -131,92 +183,67 @@ def parse_category(category_url, start_id, end_id):
     return books_urls
 
 
+def parse_book_page(book_id, base_url, book_download_url, args):
+    if not args.skip_txt:
+        book_text = get_book_text(book_download_url, book_id)
+
+        if book_text == None:
+            raise Exception('No text to download')
+
+    book_info_url = os.path.join(base_url, 'b{}/'.format(book_id))
+    book_info = get_book_info(book_info_url)
+    book_name = create_book_name(book_id, book_info)
+
+    books_path = os.path.join(args.dest_folder, 'books', '')
+    if not args.skip_txt:
+        save_text_file_to_folder(book_text, book_name, books_path)
+
+    if not args.skip_imgs:
+        book_image_path = os.path.join(args.dest_folder, 'images', '')
+        image_name = book_info['image_url'].split('/')[-1]
+        download_image(book_info['image_url'], image_name, book_image_path)
+
+    img_path = os.path.join(book_image_path, image_name)
+    book_info['img_path'] = img_path
+
+    book_path = os.path.join(books_path, book_name)
+    book_info['book_path'] = book_path
+
+    print(book_path)
+
+    return book_info
+
+
+def save_books_description(books_description, args):
+    Path(args.json_path).mkdir(parents=True, exist_ok=True)
+    books_description_path = os.path.join(args.json_path, 'books_description.json')
+
+    with open(books_description_path, "w", encoding='utf8') as my_file:
+        json.dump(books_description, my_file, ensure_ascii=False)
+
+
 def main():
     base_url = 'http://tululu.org'
     category_path_url = 'l55'
     category_url = os.path.join(base_url, category_path_url)
-
-    parser = argparse.ArgumentParser(
-        description='This script parse books from categories pages on tululu.org website'
-    )
-    parser.add_argument(
-        '--start_page',
-        type=int,
-        default=1,
-        help='id of start page books in category'
-    )
-    parser.add_argument(
-        '--end_page',
-        type=int,
-        default=get_pages_count(category_url),
-        help='id of end page books in category'
-    )
-    parser.add_argument(
-        '--dest_folder',
-        default=MEDIA_URL,
-        help='path to result of parsing'
-    )
-    parser.add_argument(
-        '--skip_imgs',
-        action='store_true',
-        help='option to allow skip download book\'s images'
-    )
-    parser.add_argument(
-        '--skip_txt',
-        action='store_true',
-        help='option to allow skip download book\'s text'
-    )
-    parser.add_argument(
-        '--json_path',
-        default=MEDIA_URL,
-        help='path to books description json file'
-    )
-
-    args = parser.parse_args()
-
-    start_id = int(args.start_page)
-    end_id = int(args.end_page)
-    books_urls = parse_category(category_url, start_id, end_id)
-
     book_download_url = os.path.join(base_url, 'txt.php')
 
+    parser = configurate_argparse(get_pages_count, category_url)
+    args = parser.parse_args()
+
+    books_urls = parse_category(category_url, args.start_page, args.end_page)
+    books_ids = get_books_ids(books_urls)
+
     books_description = []
-
-    book_ids = [books_url.split('/b')[-1][:-1] for books_url in books_urls]
-    for book_id in book_ids:
-        if not args.skip_txt:
-            book_text = get_book_text(book_download_url, book_id)
-
-            if book_text == None:
-                continue
-
-        book_info_url = os.path.join(base_url, 'b{}/'.format(book_id))
-        book_info = get_book_info(book_info_url)
-        book_name = '{}. {}.txt'.format(book_id, book_info['title_text'])
-
-        books_path = os.path.join(args.dest_folder, 'books', '')
-        if not args.skip_txt:
-            save_text_file_to_folder(book_text, book_name, books_path)
-
-        if not args.skip_imgs:
-            book_image_path = os.path.join(args.dest_folder, 'images', '')
-            image_name = book_info['image_url'].split('/')[-1]
-            download_image(book_info['image_url'], image_name, book_image_path)
-
-        img_path = os.path.join(book_image_path, image_name)
-        book_info['img_path'] = img_path
-
-        book_path = os.path.join(books_path, book_name)
-        book_info['book_path'] = book_path
+    for book_id in books_ids:
+        try:
+            book_info = parse_book_page(book_id, base_url, book_download_url, args)
+        except Exception:
+            continue
 
         books_description.append(book_info)
 
-        print(book_path)
-
-    Path(args.json_path).mkdir(parents=True, exist_ok=True)
-    books_description_path = os.path.join(args.json_path, 'books_description.json')
-    with open(books_description_path, "w", encoding='utf8') as my_file:
-        json.dump(books_description, my_file, ensure_ascii=False)
+    save_books_description(books_description, args)
 
 
 if __name__ == '__main__':
